1a55cf3e01de9616f5e8fc092e34c0dc1beed3e2
Peter <ptang3@uci.edu>
2020-03-12 13:14:15 -0700

    Attributes, constructor, and getset methods for image and javafx rectangle

--- src/TileEntity.java
+++ src/TileEntity.java
@@ -1,3 +1,55 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
 public abstract class TileEntity implements Moveable, Clearable{
-    public String iconSrc;
+    String iconSrc;
+    Image img;
+    ImageView imgV;
+    Rectangle rect;
+
+    public TileEntity(String iconSrc) {
+        this.iconSrc = iconSrc;
+        img = new Image(iconSrc);
+        imgV = new ImageView(img);
+    }
+
+    public TileEntity(double x, double y, double width, double height, String color)
+    {
+        this.rect = new Rectangle(x, y, width, height);
+        this.rect.setFill(Paint.valueOf(color));
+    }
+
+    public String getIconSrc() {
+        return iconSrc;
+    }
+
+    public void setIconSrc(String iconSrc) {
+        this.iconSrc = iconSrc;
+    }
+
+    public Image getImg() {
+        return img;
+    }
+
+    public void setImg(Image img) {
+        this.img = img;
+    }
+
+    public ImageView getImgV() {
+        return imgV;
+    }
+
+    public void setImgV(ImageView imgV) {
+        this.imgV = imgV;
+    }
+
+    public Rectangle getRect() {
+        return rect;
+    }
+
+    public void setRect(Rectangle rect) {
+        this.rect = rect;
+    }
 }
