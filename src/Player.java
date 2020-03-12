84fc73f3e68d7833ac6d610468cee014ebc9bf15
Hung Phan <17309881+hp0101@users.noreply.github.com>
2020-03-11 20:26:03 -0700

    added Session and Player class

---
+++ src/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+    public String username;
+    public Session savedSession;
+    public Session[] finishedSessions;
+    public int highScore;
+
+    public void startSession() {
+
+    }
+}
---
+++ src/Session.java
@@ -0,0 +1,16 @@
+public class Session {
+    public int score;
+    public int timeElapsed;
+
+    public void start() {
+
+    }
+
+    public void end() {
+
+    }
+
+    public void save() {
+
+    }
+}
