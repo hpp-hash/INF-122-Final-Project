1c2c1f3764bfb02ae0b280ae50ef9c0273b4b824
Hung Phan <17309881+hp0101@users.noreply.github.com>
2020-03-12 19:36:05 -0700

    added session logic

--- src/Session.java
+++ src/Session.java
@@ -1,6 +1,16 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Session {
-    public int score;
+    public int currentScore;
+    public int[] bestScoreArr;
     public int timeElapsed;
+
+    public Session() {
+        currentScore = 0;
+    }
 
     public void start() {
 
@@ -13,4 +23,27 @@ public class Session {
     public void save() {
 
     }
+
+    public void saveScore() {
+        // save currentScore to a file
+        try {
+            File scoreFile = new File("allScores.txt");
+            if (scoreFile.createNewFile()) {
+                System.out.println("File created: " + scoreFile.getName());
+                FileWriter myWriter = new FileWriter(scoreFile.getName());
+                myWriter.write(currentScore);
+                myWriter.close();
+            }
+            else {
+                System.out.println("File allScores  already exists.");
+                FileWriter newWriter = new FileWriter(scoreFile.getName());
+                newWriter.write(currentScore);
+                newWriter.close();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 }
