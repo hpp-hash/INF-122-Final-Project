d69ae975f286b8a4ffb7091a6daa27257979eccf
Derek Tran <dphtran501@gmail.com>
2020-03-12 19:24:12 -0700

    Modify TileEntityFactory and NextTileEntity to use factory method
    TileEntityFactory interface will be used to create new tile entities and NextTileEntity class will be used to create and add new tile entities to the tile map. TileEntity is modified to have a name attribute so that TileEntityFactory can create specific tile entities.

---
+++ src/NextTileEntity.java
@@ -0,0 +1,13 @@
+public class NextTileEntity {
+
+    private TileEntityFactory factory;
+
+    public NextTileEntity(TileEntityFactory factory){
+        this.factory = factory;
+    }
+
+    public void addNewTileEntity(String entityName, Tile tile){
+        factory.createTileEntity(entityName);
+        // TODO: add entity to tile if free
+    }
+}
